da101882777cc50ab4d4ef8fb93a6d35e227632a
noBIT18 <piontkowskicosiek@gmail.com>
2024-12-29 20:37:28 +0100

    ad

--- Official Game/Skripts/raycast.gd
+++ Official Game/Skripts/raycast.gd
@@ -7,9 +7,3 @@ func _process(delta: float) -> void:
 			print("Collision with:", collider)
 			if collider and collider.has_method("on_interact"):
 				collider.on_interact()
-				var flashscene = load("res://Official Game/flashlight.tscn")
-				var flashinst = flashscene.instantiate()
-				var flashnode = flashinst.get_node("./Flashlight")
-				if flashnode.has_method("picks_up"):
-					print("First")
-					flashnode.picks_up()
