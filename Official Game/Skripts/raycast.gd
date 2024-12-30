4b1f5896e7bf250cd4a9ca8c8f50576d0a50d134
noBIT18 <piontkowskicosiek@gmail.com>
2024-12-30 21:01:34 +0100

    Pointer changes

--- Official Game/Skripts/raycast.gd
+++ Official Game/Skripts/raycast.gd
@@ -1,9 +1,18 @@
 extends RayCast3D
 
+signal change_property
+
 func _process(delta: float) -> void:
+	var collider = get_collider()
+	var pointer = get_node("/root/chapter1/player/head/Camera3D/UI/CanvasLayer/Container/Screenshot2024-10-13031926")
+	if is_colliding() and collider.has_method("on_interact"):
+		pointer.scale.x = 0.7
+		pointer.scale.y = 0.7
+	else:
+		pointer.scale.y = 1
+		pointer.scale.x = 1
 	if Input.is_action_just_pressed("Interact"):
 		if is_colliding():
-			var collider = get_collider()
 			print("Collision with:", collider)
 			if collider and collider.has_method("on_interact"):
 				collider.on_interact()
