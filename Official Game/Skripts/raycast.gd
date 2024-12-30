9fe19aba08cc5f66397d926dc9f7d2fc36c0a20e
noBIT18 <piontkowskicosiek@gmail.com>
2024-12-30 18:15:04 +0100

    paar tweaks

--- Official Game/Skripts/raycast.gd
+++ Official Game/Skripts/raycast.gd
@@ -1,9 +1,9 @@
 extends RayCast3D
 
-func _process(delta: float) -> void:
+func _process(delta):
+	var collider = get_collider()
 	if Input.is_action_just_pressed("Interact"):
 		if is_colliding():
-			var collider = get_collider()
 			print("Collision with:", collider)
 			if collider and collider.has_method("on_interact"):
 				collider.on_interact()
