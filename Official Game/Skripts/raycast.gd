6456a2d7ef8eb5c6d897f1b20cb3cff493c80bc9
noBIT18 <piontkowskicosiek@gmail.com>
2025-01-03 18:14:36 +0100

    animation RP

--- Official Game/Skripts/raycast.gd
+++ Official Game/Skripts/raycast.gd
@@ -1,9 +1,33 @@
 extends RayCast3D
 
+var TIMEROVER
+@onready var Animations = $"../../AnimationPlayer"
+var Lastobject
+var collider = get_collider()
+var timer
+
+
+func use():
+	Lastobject.on_interact()
+
+func timerstart():
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 1.5
+	timer.start()
+	timer.timeout.connect(_on_Timers_timeout)
+	Animations.play("Pick Up")
+	TIMEROVER = false
+
+func _on_Timers_timeout():
+	TIMEROVER = true
+	use()
 
 
 func _process(delta):
-	var collider = get_collider()
+	if TIMEROVER:
+		remove_child(timer)
+	collider = get_collider()
 	var pointer = get_node("/root/chapter1/player/head/Camera3D/UI/CanvasLayer/Container/Sprite2D")
 	if is_colliding():
 		if collider and collider.has_method("on_interact"):
@@ -13,4 +37,5 @@ func _process(delta):
 			pointer.scale.y = 0.3
 			pointer.scale.x = 0.3
 		if collider and collider.has_method("on_interact") and Input.is_action_just_pressed("Interact"):
-			collider.on_interact()
+			timerstart()
+			Lastobject = collider
