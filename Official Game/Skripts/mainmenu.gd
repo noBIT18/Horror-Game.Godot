20fb8cefaa5d18d9e05bc960f6b117cab0b74678
noBIT18 <piontkowskicosiek@gmail.com>
2025-01-10 16:05:31 +0100

    asd

--- Official Game/Skripts/mainmenu.gd
+++ Official Game/Skripts/mainmenu.gd
@@ -2,14 +2,29 @@ extends Control
 
 @onready var PlayGame = $Button
 @onready var Quit = $Button2
+@onready var HTP = $Button3
+@onready var Back = $Button4
+@onready var HTPT = $Label2
+
+var AButtons = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	AButtons = [HTP, PlayGame, Quit]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if HTP.button_pressed:
+		for Button in AButtons:
+			Button.visible = false
+			Back.visible = true
+			HTPT.visible = true
+	if Back.button_pressed:
+		Back.visible = false
+		HTPT.visible = false
+		for Button in AButtons:
+			Button.visible = true
 	if PlayGame.button_pressed:
 		get_tree().change_scene_to_file("res://Official Game/Chapter1.tscn")
 	if Quit.button_pressed:
--- Official Game/Skripts/raycast.gd
+++ Official Game/Skripts/raycast.gd
@@ -1,15 +1,15 @@
 extends RayCast3D
 
-var TIMEROVER
 @onready var Animations = $"../../AnimationPlayer"
+
+
+var TIMEROVER
 var Lastobject
 var collider = get_collider()
 var timer
+var pointer
 
-
-func use():
-	Lastobject.on_interact()
-
+# Creates a timer and starts it
 func timerstart():
 	timer = Timer.new()
 	add_child(timer)
@@ -19,23 +19,31 @@ func timerstart():
 	Animations.play("Pick Up")
 	TIMEROVER = false
 
+# Picks up item and changes Variable TIMEOVER to true
 func _on_Timers_timeout():
 	TIMEROVER = true
-	use()
+	Lastobject.on_interact()
 
 
 func _process(delta):
+	pointer = get_node("/root/chapter1/player/head/Camera3D/UI/CanvasLayer/Container/Sprite2D")
+	collider = get_collider()
+	
+	# Variable TIMEOVER is true removes the child "timer"
 	if TIMEROVER:
 		remove_child(timer)
-	collider = get_collider()
-	var pointer = get_node("/root/chapter1/player/head/Camera3D/UI/CanvasLayer/Container/Sprite2D")
+	
+	
 	if is_colliding():
+		# When pointer on collider with function "on_interact" change the size of the pointer
 		if collider and collider.has_method("on_interact"):
 			pointer.scale.x = 0.25
 			pointer.scale.y = 0.25
 		else:
 			pointer.scale.y = 0.3
 			pointer.scale.x = 0.3
+		
+		# when the Interact button is pressed call timerstart and put collider in Lastobject
 		if collider and collider.has_method("on_interact") and Input.is_action_just_pressed("Interact"):
-			timerstart()
 			Lastobject = collider
+			timerstart()
